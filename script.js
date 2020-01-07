ae63f1daa7c6ad37347486f2c3119e6d39bb650c
Teresa Liu <kuas033@gmail.com>
2020-01-06 22:31:04 -0800

    js button click function

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+
+    $('button').on('click', function (event) {
+        event.preventDefault();
+        var cityInput = $('#cityInput').val().trim();
+        var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInput + ',us&appid=a82307c33eaf83976520ebe58ff69ff3';
+        $('.list-group').text(cityInput);
+
+        $.ajax({
+            url:queryURL,
+            method: 'GET'
+        }).then(function(response){
+            var cityName =response.name;
+            var cityTemp =response.main.temp;
+            var cityHumidity=response.main.humidity;
+            var cityWid=response.wind.speed;
+
+
+        });
+
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
