2d64d4a03629b049e730642a5daf1178bf363efc
Teresa Liu <kuas033@gmail.com>
2020-01-08 13:49:56 -0800

    add README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# HW-6-WeatherDashboard
+# Weather Dashboard
+
+## Description:
+
+This is a simple city weather dashboard.
+
+## How to use?
+
+1. User can enter a city at the search bar on the left.
+Search history will also be shown on the page.
+
+2. It will then show you the weather today. And also the forecast for the next 5 days.
+
+3. The weather detail includes : 
+- Current temperature
+- Current humidity
+- Wind Speed 
+- UV index.
+
+![weather dashboard](./Assests/weatherdashboard.png)
+
+## Credits
+1. The weather data is based on OpenWeather API (https://openweathermap.org/)
+2. CSS framework: Bootstrap (https://getbootstrap.com/)
--- script.js
+++ script.js
@@ -1,33 +1,36 @@
 $(document).ready(function () {
-
-
+    var APIkey = "a82307c33eaf83976520ebe58ff69ff3";
+    var currentWeatherAPI = 'https://api.openweathermap.org/data/2.5/weather?q=';
+    // var iconURl = iconAPI + weatherIcon + '.png';
+    
+    
+    
     $('button').on('click', function (event) {
         event.preventDefault();
         var cityInput = $('#cityInput').val().trim();
-        var currentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInput + '&appid=a82307c33eaf83976520ebe58ff69ff3';
+        var currentWeatherURL = currentWeatherAPI + cityInput + '&appid=' + APIkey;
         var newLi =$('<li>').addClass('list-group-item');
         $('.list-group').prepend(newLi);
         newLi.text(cityInput);
-        console.log(currentWeatherURL);
-       
-
-        // $.ajax({
-        //     url: currentWeatherURL,
-        //     method: 'GET'
-        // }).then(function (response) {
-        //     var cityName = response.name;
-        //     var cityTemp = response.main.temp;
-        //     var cityHumidity = response.main.humidity;
-        //     var cityWind = response.wind.speed;
-        //     var weatherIcon = response.weather[]
+        console.log(currentWeatherAPI);
         
-            
+        
+        
+        $.ajax({
+            url: currentWeatherURL,
+            method: 'GET'
+        }).then(function (response) {
+            var cityName = response.name;
+            var cityTemp = response.main.temp;
+            var cityHumidity = response.main.humidity;
+            var cityWind = response.wind.speed;
+            var weatherIcon = response.weather[0].icon;
 
-        // });
+        });
         
     });
 
-
+    
 
 
 
