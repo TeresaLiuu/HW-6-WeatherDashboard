18bd8c8e0c2e897fc9e231802654772533b2fe2a
Teresa Liu <kuas033@gmail.com>
2020-01-07 18:54:10 -0800

    update city search history

--- script.js
+++ script.js
@@ -4,25 +4,28 @@ $(document).ready(function () {
     $('button').on('click', function (event) {
         event.preventDefault();
         var cityInput = $('#cityInput').val().trim();
-        var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInput + ',us&appid=a82307c33eaf83976520ebe58ff69ff3';
-        $('.list-group').text(cityInput);
+        var currentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityInput + '&appid=a82307c33eaf83976520ebe58ff69ff3';
+        var newLi =$('<li>').addClass('list-group-item');
+        $('.list-group').prepend(newLi);
+        newLi.text(cityInput);
+        console.log(currentWeatherURL);
+       
 
-        $.ajax({
-            url: queryURL,
-            method: 'GET'
-        }).then(function (response) {
-            var cityName = response.name;
-            var cityTemp = response.main.temp;
-            var cityHumidity = response.main.humidity;
-            var cityWind = response.wind.speed;
-            $('#tempInput').text(cityName + cityTemp + cityHumidity + cityWind);
+        // $.ajax({
+        //     url: currentWeatherURL,
+        //     method: 'GET'
+        // }).then(function (response) {
+        //     var cityName = response.name;
+        //     var cityTemp = response.main.temp;
+        //     var cityHumidity = response.main.humidity;
+        //     var cityWind = response.wind.speed;
+        //     var weatherIcon = response.weather[]
+        
+            
 
-
-        });
-
-
+        // });
+        
     });
-
 
 
 
